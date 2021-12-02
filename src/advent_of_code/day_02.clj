7ffa13a82af164ad15f8bdfa8093cb52e833f319
Osvalds Neiders <osvalds.neiders@ui.com>
2021-12-02 10:43:29 +0200

    Day 2

--- src/advent_of_code/day_02.clj
+++ src/advent_of_code/day_02.clj
@@ -1,11 +1,33 @@
-(ns advent-of-code.day-02)
+(ns advent-of-code.day-02
+  (:require [clojure.string :as s]))
+
+(defn parse-input-line [line]
+  (let [[direction distance] (s/split line #" ")]
+    [direction (Integer/parseInt distance)]))
+
+(defn direction-sum [grouped-directions direction]
+  (apply + (map second (get grouped-directions direction))))
 
 (defn part-1
   "Day 02 Part 1"
   [input]
-  input)
+  (let [parsed (map parse-input-line (s/split-lines input))
+        directions (group-by first parsed)
+        forward-sum (direction-sum directions "forward")
+        down-sum (direction-sum directions "down")
+        up-sum (direction-sum directions "up")]
+    (* forward-sum (- down-sum up-sum))))
+
 
 (defn part-2
   "Day 02 Part 2"
   [input]
-  input)
+  (let [parsed (map parse-input-line (s/split-lines input))
+        [_ h d] (reduce (fn [[aim horizontal depth] [direction x]]
+                          (condp = direction
+                            "down" [(+ aim x) horizontal depth]
+                            "up" [(- aim x) horizontal depth]
+                            "forward" [aim (+ horizontal x) (+ depth (* aim x))]))
+                  [0 0 0]
+                  parsed)]
+    (* h d)))
--- test/advent_of_code/day_02_test.clj
+++ test/advent_of_code/day_02_test.clj
@@ -4,9 +4,20 @@
             [clojure.java.io :refer [resource]]))
 
 (deftest part1
-  (let [expected nil]
-    (is (= expected (part-1 (slurp (resource "day-02-example.txt")))))))
+  (testing "Example"
+    (let [expected 150]
+      (is (= expected (part-1 (slurp (resource "day-02-example.txt")))))))
+
+  (testing "Real input"
+    (let [expected 150]
+      (is (= expected (part-1 (slurp (resource "day-02.txt"))))))))
+
 
 (deftest part2
-  (let [expected nil]
-    (is (= expected (part-2 (slurp (resource "day-02-example.txt")))))))
+  (testing "Example"
+    (let [expected 900]
+      (is (= expected (part-2 (slurp (resource "day-02-example.txt")))))))
+
+  (testing "Real input"
+    (let [expected 1568138742]
+      (is (= expected (part-2 (slurp (resource "day-02.txt"))))))))
