7ef1b66688a5a902c783b0037e4e5a439a03623d
Osvalds Neiders <osvalds.neiders@ui.com>
2021-12-05 17:04:33 +0200

    Day 5

--- src/advent_of_code/day_05.clj
+++ src/advent_of_code/day_05.clj
@@ -1,11 +1,102 @@
-(ns advent-of-code.day-05)
+(ns advent-of-code.day-05
+  (:require [clojure.string :as s]
+            [clojure.pprint :refer [pprint]]))
+
+(defn vec2d [x y val] (vec (repeat y (vec (repeat x val)))))
+
+(defn get-xy [xys]
+  (let [[x y] (s/split xys #",")]
+    [(Integer/parseInt x) (Integer/parseInt y)]))
+
+(defn parse-line [line-string]
+  (let [[p1 p2] (s/split line-string #"\s->\s")]
+    [(get-xy p1) (get-xy p2)]))
+
+(defn hor? [[[_ y1] [_ y2]]] (= y1 y2))
+
+(defn ver? [[[x1] [x2]]] (= x1 x2))
+
+;; H to The Izzo
+(defn is-hov? [[[x1 y1] [x2 y2]]] (or (= x1 x2) (= y1 y2)))
+
+(defn abs [x]
+  (Math/abs ^int x))
+
+(defn is-diag? [[[x1 y1] [x2 y2]]] (or
+                                     (= (+ x1 y1) (+ x2 y2))
+                                     (= (abs (- x2 y2)) (abs (- x1 y1)))))
+
+
+(defn verstappen [[x y] [mx my]] [(max x mx) (max y my)])
+
+(defn max-coord [line mp]
+  (reduce (fn [p m] (verstappen p m)) mp line))
+
+(defn sort-coord [line] (vec (if (hor? line) (sort-by first line) (sort-by second line))))
+
+(defn get-legit-input [lines legit?]
+  (reduce (fn [result line]
+            (let [[plines max] result
+                  pline (parse-line line)]
+              (if (legit? pline)
+                [(conj plines pline) (max-coord pline max)]
+                result)))
+    [[] [0 0]]
+    lines))
+
+(defn update-hline [line x1 x2]
+  (let [[start end] (if (< x1 x2) [x1 x2] [x2 x1])]
+    (vec (map-indexed (fn [idx val] (if (<= start idx end) (inc val) val)) line))))
+
+(defn update-hor [board [[x1 y1] [x2 y2]]]
+  (map-indexed (fn [idx line]
+                 (if (= idx y1)
+                   (update-hline line x1 x2)
+                   line))
+    board))
+
+(defn update-ver [board [[x1 y1] [x2 y2]]]
+  (let [[start end] (if (< y1 y2) [y1 y2] [y2 y1])]
+    (map-indexed (fn [idx line]
+                   (if (<= start idx end)
+                     (update line x1 inc)
+                     line))
+      board)))
 
 (defn part-1
   "Day 05 Part 1"
   [input]
-  input)
+  (let [[hv-lines [mx my]] (get-legit-input (s/split-lines input) is-hov?)
+        start (vec2d (inc mx) (inc my) 0)
+        board (reduce
+                (fn [result line]
+                  (if (hor? line)
+                    (update-hor result line)
+                    (update-ver result line)))
+                start
+                hv-lines)]
+    (count (filter #(> % 1) (apply concat board)))))
+
+(defn update-diag [board input-line]
+  (let [[[x1 y1] [x2 y2]] (vec (sort-by second input-line))
+        xrange (if (<= x1 x2) (range x1 (inc x2)) (range x1 (dec x2) -1))]
+    (map-indexed (fn [idx line]
+                   (if (<= y1 idx y2)
+                     (update line (nth xrange (abs (- idx y1))) inc)
+                     line))
+      board)))
 
 (defn part-2
   "Day 05 Part 2"
   [input]
-  input)
+  (let [[hvd-lines [mx my]] (get-legit-input (s/split-lines input) #(or (is-hov? %) (is-diag? %)))
+        start (vec2d (inc mx) (inc my) 0)
+        board (reduce
+                (fn [result line]
+                  (cond
+                    (hor? line) (update-hor result line)
+                    (ver? line) (update-ver result line)
+                    :else (update-diag result line)))
+                start
+                hvd-lines)]
+    (count (filter #(> % 1) (apply concat board)))))
--- test/advent_of_code/day_05_test.clj
+++ test/advent_of_code/day_05_test.clj
@@ -1,12 +1,118 @@
 (ns advent-of-code.day-05-test
   (:require [clojure.test :refer [deftest testing is]]
-            [advent-of-code.day-05 :refer [part-1 part-2]]
+            [advent-of-code.day-05 :refer :all]
             [clojure.java.io :refer [resource]]))
 
 (deftest part1
-  (let [expected nil]
-    (is (= expected (part-1 (slurp (resource "day-05-example.txt")))))))
+
+  (testing "line-parsing"
+    (let [input "3,4 -> 1,4"
+          expected [[3 4] [1 4]]]
+      (is (= expected (parse-line input)))))
+
+  (testing "is horizontal"
+    (let [input [[3 4] [1 4]]]
+      (is (= true (is-hov? input)))))
+
+  (testing "is horizontal"
+    (let [input [[4 1] [4 1]]]
+      (is (= true (is-hov? input)))))
+
+  (testing "verstappen"
+    (let [input [[1 4] [5 1]]
+          max [0 0]
+          result [5 4]]
+      (is (= result (max-coord input max)))))
+
+  (testing "vec2d"
+    (let [result [[0 0 0] [0 0 0] [0 0 0]]]
+      (is (= result (vec2d 3 3 0)))))
+
+  (testing "update-h-line"
+    (let [input [0 0 0 0 0]
+          result [0 1 1 1 0]]
+      (is (= result (update-hline input 1 3)))))
+
+  (testing "sorting"
+    (let [input [[9 4] [3 4]]]
+      (is (= [[3 4] [9 4]] (sort-coord input)))))
+
+  (testing "example solution"
+    (let [expected 5]
+      (is (= expected (part-1 (slurp (resource "day-05-example.txt")))))))
+
+  (testing "solution p1"
+    (let [expected 5197]
+      (is (= expected (part-1 (slurp (resource "day-05.txt"))))))))
 
 (deftest part2
-  (let [expected nil]
-    (is (= expected (part-2 (slurp (resource "day-05-example.txt")))))))
+  (testing "[[8 0] [0 8]]"
+    (let [empty-start (vec2d 10 10 0)
+          line [[8 0] [0 8]]
+          result '([0 0 0 0 0 0 0 0 1 0]
+                   [0 0 0 0 0 0 0 1 0 0]
+                   [0 0 0 0 0 0 1 0 0 0]
+                   [0 0 0 0 0 1 0 0 0 0]
+                   [0 0 0 0 1 0 0 0 0 0]
+                   [0 0 0 1 0 0 0 0 0 0]
+                   [0 0 1 0 0 0 0 0 0 0]
+                   [0 1 0 0 0 0 0 0 0 0]
+                   [1 0 0 0 0 0 0 0 0 0]
+                   [0 0 0 0 0 0 0 0 0 0])]
+      (is (= result (update-diag empty-start line)))))
+
+  (testing "[[0 0] [8 8]]"
+    (let [empty-start (vec2d 10 10 0)
+          line [[0 0] [8 8]]
+          result '([1 0 0 0 0 0 0 0 0 0]
+                   [0 1 0 0 0 0 0 0 0 0]
+                   [0 0 1 0 0 0 0 0 0 0]
+                   [0 0 0 1 0 0 0 0 0 0]
+                   [0 0 0 0 1 0 0 0 0 0]
+                   [0 0 0 0 0 1 0 0 0 0]
+                   [0 0 0 0 0 0 1 0 0 0]
+                   [0 0 0 0 0 0 0 1 0 0]
+                   [0 0 0 0 0 0 0 0 1 0]
+                   [0 0 0 0 0 0 0 0 0 0])]
+      (is (= result (update-diag empty-start line)))))
+
+  (testing "[[5 5] [8 2]]"
+    (let [empty-start (vec2d 10 10 0)
+          line [[5 5] [8 2]]
+          result '([0 0 0 0 0 0 0 0 0 0]
+                   [0 0 0 0 0 0 0 0 0 0]
+                   [0 0 0 0 0 0 0 0 1 0]
+                   [0 0 0 0 0 0 0 1 0 0]
+                   [0 0 0 0 0 0 1 0 0 0]
+                   [0 0 0 0 0 1 0 0 0 0]
+                   [0 0 0 0 0 0 0 0 0 0]
+                   [0 0 0 0 0 0 0 0 0 0]
+                   [0 0 0 0 0 0 0 0 0 0]
+                   [0 0 0 0 0 0 0 0 0 0])]
+      (is (= result (update-diag empty-start line)))))
+
+  (testing "[[6 4] [2 0]]"
+    (let [empty-start (vec2d 10 10 0)
+          line [[6 4] [2 0]]
+          result '([0 0 1 0 0 0 0 0 0 0]
+                   [0 0 0 1 0 0 0 0 0 0]
+                   [0 0 0 0 1 0 0 0 0 0]
+                   [0 0 0 0 0 1 0 0 0 0]
+                   [0 0 0 0 0 0 1 0 0 0]
+                   [0 0 0 0 0 0 0 0 0 0]
+                   [0 0 0 0 0 0 0 0 0 0]
+                   [0 0 0 0 0 0 0 0 0 0]
+                   [0 0 0 0 0 0 0 0 0 0]
+                   [0 0 0 0 0 0 0 0 0 0])]
+      (is (= result (update-diag empty-start line)))))
+
+  (testing "is-diag?"
+    (is (= true (is-diag? [[1 1] [3 3]])))
+    (is (= false (is-diag? [[2 2] [2 1]]))))
+
+  (testing "example"
+    (let [expected 12]
+      (is (= expected (part-2 (slurp (resource "day-05-example.txt")))))))
+
+  (let [expected 18605]
+    (is (= expected (part-2 (slurp (resource "day-05.txt")))))))
