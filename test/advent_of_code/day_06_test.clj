0023e0974b3b446ee27ce7b97495fa6b545f2119
Osvalds Neiders <osvalds.neiders@ui.com>
2021-12-06 14:23:41 +0200

    Day 6

--- test/advent_of_code/day_06_test.clj
+++ test/advent_of_code/day_06_test.clj
@@ -1,12 +1,38 @@
 (ns advent-of-code.day-06-test
   (:require [clojure.test :refer [deftest testing is]]
-            [advent-of-code.day-06 :refer [part-1 part-2]]
+            [advent-of-code.day-06 :refer :all]
             [clojure.java.io :refer [resource]]))
 
 (deftest part1
-  (let [expected nil]
-    (is (= expected (part-1 (slurp (resource "day-06-example.txt")))))))
+
+  (testing "example day 18"
+    (let [expected 26]
+      (is (= expected (part-1 (slurp (resource "day-06-example.txt")) 18)))))
+
+  (testing "example day 80"
+    (let [expected 5934]
+      (is (= expected (part-1 (slurp (resource "day-06-example.txt")) 80)))))
+
+
+  (testing "soļušion day 80"
+    (let [expected 5934]
+      (is (= expected (part-1 (slurp (resource "day-06.txt")) 80))))))
+
 
 (deftest part2
-  (let [expected nil]
-    (is (= expected (part-2 (slurp (resource "day-06-example.txt")))))))
+
+  (testing "exmaple"
+    (let [expected 26]
+      (is (= expected (part-2 (slurp (resource "day-06-example.txt")) 18)))))
+
+  (testing "exmaple"
+    (let [expected 5934]
+      (is (= expected (part-2 (slurp (resource "day-06-example.txt")) 80)))))
+
+  (testing "example pt2"
+    (let [expected 26984457539]
+      (is (= expected (part-2 (slurp (resource "day-06-example.txt")) 256)))))
+
+  (testing "example pt2"
+    (let [expected 1644286074024]
+      (is (= expected (part-2 (slurp (resource "day-06.txt")) 256))))))
