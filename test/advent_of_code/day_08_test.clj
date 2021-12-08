e1b25ad6a8950fe1543e95aa6a6a1c9d8e37e948
Osvalds Neiders <osvalds.neiders@ui.com>
2021-12-08 21:53:38 +0200

    Day 8

--- test/advent_of_code/day_08_test.clj
+++ test/advent_of_code/day_08_test.clj
@@ -4,9 +4,19 @@
             [clojure.java.io :refer [resource]]))
 
 (deftest part1
-  (let [expected nil]
-    (is (= expected (part-1 (slurp (resource "day-08-example.txt")))))))
+  (testing "example"
+    (let [expected 26]
+      (is (= expected (part-1 (slurp (resource "day-08-example.txt")))))))
+
+  (testing "soļution"
+    (let [expected 26]
+      (is (= expected (part-1 (slurp (resource "day-08.txt"))))))))
 
 (deftest part2
-  (let [expected nil]
-    (is (= expected (part-2 (slurp (resource "day-08-example.txt")))))))
+  (testing "example"
+    (let [expected 61229]
+      (is (= expected (part-2 (slurp (resource "day-08-example.txt")))))))
+
+  (testing "soļution"
+    (let [expected 61229]
+      (is (= expected (part-2 (slurp (resource "day-08.txt"))))))))
