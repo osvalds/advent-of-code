a49c443a9c7e4bbc277185e2d6e56e176ba28cd1
Osvalds Neiders <osvalds.neiders@ui.com>
2021-12-12 14:08:12 +0200

    Day 12 slow AF

--- src/advent_of_code/y2021/day_12.clj
+++ src/advent_of_code/y2021/day_12.clj
@@ -1,11 +1,78 @@
-(ns advent-of-code.y2021.day-12)
+(ns advent-of-code.y2021.day-12
+  (:require [clojure.string :as str]))
+
+(defn connected-vertices [edges vertex]
+  (reduce
+    (fn [result [A B]]
+      (if (or (= A vertex) (= B vertex))
+        (conj result (if (= A vertex) B A))
+        result))
+    []
+    edges))
+
+(defn input->graph [input]
+  (let [edges (map #(str/split % #"-") (str/split-lines input))
+        vertices (into #{} (flatten edges))]
+    (reduce
+      (fn [graph vertex]
+        (assoc graph vertex (connected-vertices edges vertex)))
+      {}
+      vertices)))
+
+(defn can-add-p1? [current-path node]
+  (or (every? #(Character/isUpperCase ^char %) node)
+    (not (some #(= node %) current-path))))
+
+(defn add-new-paths-to-queue [next-q graph current-path can-add?]
+  (let [last-node (last current-path)
+        adj-nodes (filter #(can-add? current-path %) (get graph last-node))
+        new-paths (mapv #(conj current-path %) adj-nodes)]
+    (into next-q new-paths)))
+
+(defn process-q [graph q paths end can-add?]
+  (let [current-path (first q)
+        next-q (vec (rest q))]
+    (if (= end (last current-path))
+      [(conj paths current-path) next-q]
+      [paths (add-new-paths-to-queue next-q graph current-path can-add?)])))
+
+(defn all-paths [graph start end can-add?]
+  (loop [q [[start]]
+         paths []]
+    (if (empty? q)
+      paths
+      (let [[next-paths next-q] (process-q graph q paths end can-add?)]
+        (recur next-q next-paths)))))
 
 (defn part-1
   "Day 12 Part 1"
   [input]
-  input)
+  (let [graph (input->graph input)]
+    (count (all-paths graph "start" "end" can-add-p1?))))
+
+(defn can-add-p2? [can-be-twice current-path node]
+  (let [can-add? (and (= node can-be-twice) (< (count (filter #(= % node) current-path)) 2))
+        result (or
+                 (every? #(Character/isUpperCase ^char %) node)
+                 (or
+                   can-add?
+                   (not (some #(= node %) current-path))))]
+    result))
+
+(defn get-smalls [graph]
+  (filter
+    (fn [node]
+      (and (not (or (= "end" node) (= "start" node)))
+        (every? #(Character/isLowerCase ^char %) node)))
+    (keys graph)))
 
 (defn part-2
   "Day 12 Part 2"
   [input]
-  input)
+  (let [graph (input->graph input)]
+    (->> graph
+      (get-smalls)
+      (map #(all-paths graph "start" "end" (partial can-add-p2? %)))
+      (apply concat)
+      (into #{})
+      (count))))
--- test/advent_of_code/y2021/day_12_test.clj
+++ test/advent_of_code/y2021/day_12_test.clj
@@ -1,12 +1,50 @@
 (ns advent-of-code.y2021.day-12-test
   (:require [clojure.test :refer [deftest testing is]]
-            [advent-of-code.y2021.day-12 :refer [part-1 part-2]]
+            [advent-of-code.y2021.day-12 :refer :all]
             [clojure.java.io :refer [resource]]))
 
 (deftest part1
-  (let [expected nil]
-    (is (= expected (part-1 (slurp (resource "2021/day-12-example.txt")))))))
+  (testing "can add part1"
+    (let [path ["start" "A" "b" "A" "c"]]
+      (is (= true (can-add-p1? path "end")))
+      (is (= true (can-add-p1? path "XYZ")))
+      (is (= false (can-add-p1? path "b")))
+      (is (= false (can-add-p1? path "start")))
+      (is (= true (can-add-p1? path "A")))))
+
+  (testing "basic"
+    (let [expected 10]
+      (is (= expected (part-1 (slurp (resource "2021/day-12-example.txt")))))))
+
+  (testing "larger"
+    (let [expected 19]
+      (is (= expected (part-1 (slurp (resource "2021/day-12-example-larger.txt")))))))
+
+  (testing "even larger"
+    (let [expected 226]
+      (is (= expected (part-1 (slurp (resource "2021/day-12-example-even-larger.txt")))))))
+
+  (testing "solution"
+    (let [expected 5157]
+      (is (= expected (part-1 (slurp (resource "2021/day-12.txt"))))))))
+
 
 (deftest part2
-  (let [expected nil]
-    (is (= expected (part-2 (slurp (resource "2021/day-12-example.txt")))))))
+  (testing "can-add-part2"
+    (let [path ["start" "A" "b" "A" "c"]]
+      (is (= false (can-add-p2? "b" path "start")))
+      (is (= true (can-add-p2? "b" path "b")))
+      (is (= true (can-add-p2? "b" path "A")))
+      (is (= false (can-add-p2? "b" path "c")))
+      (is (= true (can-add-p2? "b" path "end"))))
+
+    (let [path ["start" "A" "b" "A" "c" "end"]]
+      (is (= false (can-add-p2? "b" path "end")))))
+
+  (testing "example"
+    (let [expected 36]
+      (is (= expected (part-2 (slurp (resource "2021/day-12-example.txt")))))))
+
+  (testing "solution"
+    (let [expected 144309]
+      (is (= expected (part-2 (slurp (resource "2021/day-12.txt"))))))))
