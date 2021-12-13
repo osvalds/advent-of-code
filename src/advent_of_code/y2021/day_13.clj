2ab77519df2f91df1ba99a763ef6dd7b5b94e9f1
Osvalds Neiders <osvalds.neiders@ui.com>
2021-12-13 11:00:06 +0200

    Day 13

--- src/advent_of_code/y2021/day_13.clj
+++ src/advent_of_code/y2021/day_13.clj
@@ -1,11 +1,64 @@
-(ns advent-of-code.y2021.day-13)
+(ns advent-of-code.y2021.day-13
+  (:require [clojure.string :as str]))
+
+(defn vec2d [x y val] (vec (repeat y (vec (repeat x val)))))
+
+(defn parse-coord [c] (mapv #(Integer/parseInt %) c))
+
+(defn parse-fold [f]
+  (let [[axis pos] (str/split f #"=")]
+    [axis (Integer/parseInt pos)]))
+
+(defn parse [input]
+  (let [lines (str/split-lines input)
+        split-position (.indexOf lines "")
+        [coords folds] (split-at split-position lines)]
+    [(map #(parse-coord (str/split % #",")) coords)
+     (map #(parse-fold (last (str/split % #" "))) (rest folds))]))
+
+(defn get-dimensions [coords]
+  [(inc (apply max (map first coords))) (inc (apply max (map second coords)))])
+
+(defn do-fold [coordinates [axis fold-pos]]
+  (if (= axis "x")
+    (map
+      (fn [[x y]]
+        (if (< fold-pos x)
+          [(- fold-pos (- x fold-pos)) y]
+          [x y]))
+      coordinates)
+    (map
+      (fn [[x y]]
+        (if (< fold-pos y)
+          [x (- fold-pos (- y fold-pos))]
+          [x y]))
+      coordinates)))
 
 (defn part-1
   "Day 13 Part 1"
   [input]
-  input)
+  (let [[coordinates folds] (parse input)]
+    (count (into #{} (do-fold coordinates (first folds))))))
+
+(defn print-key [coords]
+  (let [[maxx maxy] (get-dimensions coords)
+        printer (vec2d maxx maxy ".")
+        final-board (reduce
+                      (fn [board [x y]]
+                        (assoc-in board [y x] "#"))
+                      printer
+                      coords)]
+    (println (str/join "\n" (map #(str/join %) final-board)))))
 
 (defn part-2
   "Day 13 Part 2"
   [input]
-  input)
+  (let [[coordinates folds] (parse input)
+        final-coords (reduce
+                       (fn [coords fold]
+                         (into #{} (do-fold coords fold)))
+                       (into #{} coordinates)
+                       folds)]
+    (print-key final-coords)))
+
+
--- test/advent_of_code/y2021/day_13_test.clj
+++ test/advent_of_code/y2021/day_13_test.clj
@@ -1,12 +1,17 @@
 (ns advent-of-code.y2021.day-13-test
   (:require [clojure.test :refer [deftest testing is]]
-            [advent-of-code.y2021.day-13 :refer [part-1 part-2]]
+            [advent-of-code.y2021.day-13 :refer :all]
             [clojure.java.io :refer [resource]]))
 
 (deftest part1
-  (let [expected nil]
-    (is (= expected (part-1 (slurp (resource "2021/day-13-example.txt")))))))
+  (testing "example"
+    (let [expected 17]
+      (is (= expected (part-1 (slurp (resource "2021/day-13-example.txt")))))))
+
+  (testing "soļušon"
+    (let [expected 675]
+      (is (= expected (part-1 (slurp (resource "2021/day-13.txt"))))))))
 
 (deftest part2
   (let [expected nil]
-    (is (= expected (part-2 (slurp (resource "2021/day-13-example.txt")))))))
+    (is (= expected (part-2 (slurp (resource "2021/day-13.txt")))))))
