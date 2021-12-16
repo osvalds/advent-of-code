6b9bc9b34a4ffca50c15b047bdcacecb3200cb61
Osvalds Neiders <osvalds.neiders@ui.com>
2021-12-16 22:01:54 +0200

    2021 Day 16

--- src/advent_of_code/y2021/day_16.clj
+++ src/advent_of_code/y2021/day_16.clj
@@ -1,11 +1,99 @@
-(ns advent-of-code.y2021.day-16)
+(ns advent-of-code.y2021.day-16
+  (:require [clojure.string :as str]))
+
+(defn parse-int
+  ([s] (Long/parseUnsignedLong s))
+  ([s r] (Long/parseUnsignedLong s r)))
+
+(def h->b {\0 "0000"
+           \1 "0001"
+           \2 "0010"
+           \3 "0011"
+           \4 "0100"
+           \5 "0101"
+           \6 "0110"
+           \7 "0111"
+           \8 "1000"
+           \9 "1001"
+           \A "1010"
+           \B "1011"
+           \C "1100"
+           \D "1101"
+           \E "1110"
+           \F "1111"})
+
+(defn hex->binary [hex-str]
+  (apply str (map #(get h->b %) hex-str)))
+
+(defn decode-number [number-part start-index]
+  (loop [i 0
+         result ""]
+    (let [segment (subs number-part i (+ i 5))]
+      (if (= \1 (first segment))
+        (recur (+ i 5) (str result (subs segment 1 5)))
+        [(parse-int (str result (subs segment 1 5)) 2) (+ start-index i 5)]))))
+
+(defn decode [bin-str index]
+  (let [version (parse-int (subs bin-str index (+ index 3)) 2)
+        type-id (parse-int (subs bin-str (+ index 3) (+ index 6)) 2)]
+    (if (= type-id 4)
+      (let [[val next-start] (decode-number (subs bin-str (+ index 6)) (+ index 6))]
+        [next-start
+         {:version version
+          :type-id type-id
+          :value   val}])
+      (let [length-type (get bin-str (+ index 6))]
+        (if (= \0 length-type)
+          (let [subpackets-length (parse-int (subs bin-str (+ index 7) (+ index 7 15)) 2)]
+            [(+ index 7 15 subpackets-length)
+             {:version    version
+              :type-id    type-id
+              :subpackets (loop [packets []
+                                 i (+ index 7 15)]
+                            (let [[next-start packet] (decode bin-str i)]
+                              (if (= subpackets-length (- next-start (+ index 7 15)))
+                                (conj packets packet)
+                                (recur (conj packets packet) next-start))))}])
+
+          (let [subpacket-count (parse-int (subs bin-str (+ index 7) (+ index 7 11)) 2)]
+            (let [[next-index subpackets] (loop [packets []
+                                                 iteration 0
+                                                 i (+ index 7 11)]
+                                            (if (< iteration subpacket-count)
+                                              (let [[next-start packet] (decode bin-str i)]
+                                                (recur (conj packets packet) (inc iteration) next-start))
+                                              [i packets]))]
+              [next-index
+               {:version    version
+                :type-id    type-id
+                :subpackets subpackets}])))))))
+
+(defn sum-versions [decoded-packets sum]
+  (if (not (:subpackets decoded-packets))
+    (+ sum (:version decoded-packets))
+    (reduce
+      (fn [result packet]
+        (sum-versions packet result))
+      (+ sum (:version decoded-packets))
+      (:subpackets decoded-packets))))
+
+(defn calculate [packet]
+  (condp = (:type-id packet)
+    0 (apply + (map calculate (:subpackets packet)))
+    1 (apply * (map calculate (:subpackets packet)))
+    2 (apply min (map calculate (:subpackets packet)))
+    3 (apply max (map calculate (:subpackets packet)))
+    4 (:value packet)
+    5 (if (apply > (map calculate (:subpackets packet))) 1 0)
+    6 (if (apply < (map calculate (:subpackets packet))) 1 0)
+    7 (if (apply = (map calculate (:subpackets packet))) 1 0)))
 
 (defn part-1
   "Day 16 Part 1"
   [input]
-  input)
+  (-> input (hex->binary) (decode 0) (second) (sum-versions 0)))
 
 (defn part-2
   "Day 16 Part 2"
   [input]
-  input)
+  (-> input (hex->binary) (decode 0) (second) (calculate)))
--- test/advent_of_code/y2021/day_16_test.clj
+++ test/advent_of_code/y2021/day_16_test.clj
@@ -1,12 +1,57 @@
 (ns advent-of-code.y2021.day-16-test
   (:require [clojure.test :refer [deftest testing is]]
-            [advent-of-code.y2021.day-16 :refer [part-1 part-2]]
+            [advent-of-code.y2021.day-16 :refer :all]
             [clojure.java.io :refer [resource]]))
 
 (deftest part1
-  (let [expected nil]
-    (is (= expected (part-1 (slurp (resource "2021/day-16-example.txt")))))))
+  (testing "hex str to binary string"
+    (let [input "38006F45291200"
+          expected "00111000000000000110111101000101001010010001001000000000"]
+      (is (= expected (hex->binary input))))
+
+    (let [input "EE00D40C823060"
+          expected "11101110000000001101010000001100100000100011000001100000"]
+      (is (= expected (hex->binary input))))
+
+    (let [input "D2FE28"
+          expected "110100101111111000101000"]
+      (is (= expected (hex->binary input)))))
+
+  (testing "8A004A801A8002F478"
+    (let [input (hex->binary "8A004A801A8002F478")
+          expected 16]
+      (is (= expected (sum-versions (second (decode input 0)) 0)))))
+
+  (testing "620080001611562C8802118E34"
+    (let [input (hex->binary "620080001611562C8802118E34")
+          expected 12]
+      (is (= expected (sum-versions (second (decode input 0)) 0)))))
+
+  (testing "C0015000016115A2E0802F182340"
+    (let [input (hex->binary "C0015000016115A2E0802F182340")
+          expected 23]
+      (is (= expected (sum-versions (second (decode input 0)) 0)))))
+
+  (testing "A0016C880162017C3686B18A3D4780"
+    (let [input (hex->binary "A0016C880162017C3686B18A3D4780")
+          expected 31]
+      (is (= expected (sum-versions (second (decode input 0)) 0)))))
+
+  (testing "011100101011110110000000110001010000"
+    (is (= 30800350288
+          (parse-int "011100101011110110000000110001010000" 2))))
+
+  (let [expected 986]
+    (is (= expected (part-1 (slurp (resource "2021/day-16.txt")))))))
 
 (deftest part2
-  (let [expected nil]
-    (is (= expected (part-2 (slurp (resource "2021/day-16-example.txt")))))))
+  (let [expected 3
+        result (part-2 "C200B40A82")
+        _ (println result)]
+    (is (= expected (part-2 "C200B40A82"))))
+
+  (let [expected 54]
+    (is (= expected (part-2 "04005AC33890"))))
+
+  (let [expected 18234816469452]
+    (is (= expected (part-2 (slurp (resource "2021/day-16.txt")))))))
