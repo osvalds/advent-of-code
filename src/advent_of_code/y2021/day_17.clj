7d6a42d220b80ac17b1cc9cb513e05e173650b75
Osvalds Neiders <osvalds.neiders@ui.com>
2021-12-17 10:46:36 +0200

    2021 day 17

--- src/advent_of_code/y2021/day_17.clj
+++ src/advent_of_code/y2021/day_17.clj
@@ -1,11 +1,47 @@
 (ns advent-of-code.y2021.day-17)
 
+(defn parse [input]
+  (->> input
+    (re-seq #"\-{0,1}[0-9]+")
+    (map #(Integer/parseInt %))
+    (partition 2)
+    (mapv vec)))
+
+(defn sum [n] (/ (* n (+ n 1)) 2))
+
+(defn vx-range [[x-min x-max]]
+  (loop [x 1]
+    (if (<= x-min (sum x) x-max)
+      (vec (range x (+ x-max 1)))
+      (recur (inc x)))))
+
+(defn vy-range [[y-min _]]
+  (vec (range y-min (- y-min))))
+
 (defn part-1
   "Day 17 Part 1"
   [input]
-  input)
+  (let [[[x-min x-max] [y-min y-max]] (parse input)]
+    (sum (- (Math/abs ^int y-min) 1))))
+
+(defn intersects-trench? [[vx-start vy-start] [x-min x-max] [y-min y-max]]
+  (loop [x 0
+         y 0
+         vx vx-start
+         vy vy-start]
+    (let [new-x (+ x vx)
+          new-y (+ y vy)]
+      (if (or (> new-x x-max) (< new-y y-min))
+        false
+        (if (and (<= x-min new-x x-max) (<= y-min new-y y-max))
+          true
+          (recur new-x new-y (max 0 (dec vx)) (dec vy)))))))
 
 (defn part-2
   "Day 17 Part 2"
   [input]
-  input)
+  (let [[[x-min x-max] [y-min y-max]] (parse input)
+        vx-range (vx-range [x-min x-max])
+        vy-range (vy-range [y-min y-max])
+        all-start-velocities (for [x vx-range y vy-range] [x y])]
+    (count (filter #(intersects-trench? % [x-min x-max] [y-min y-max]) all-start-velocities))))
--- test/advent_of_code/y2021/day_17_test.clj
+++ test/advent_of_code/y2021/day_17_test.clj
@@ -1,12 +1,22 @@
 (ns advent-of-code.y2021.day-17-test
   (:require [clojure.test :refer [deftest testing is]]
-            [advent-of-code.y2021.day-17 :refer [part-1 part-2]]
+            [advent-of-code.y2021.day-17 :refer :all]
             [clojure.java.io :refer [resource]]))
 
 (deftest part1
-  (let [expected nil]
-    (is (= expected (part-1 (slurp (resource "2021/day-17-example.txt")))))))
+  (let [expected 45]
+    (is (= expected (part-1 (slurp (resource "2021/day-17-example.txt"))))))
+
+  (let [expected 25200]
+    (is (= expected (part-1 (slurp (resource "2021/day-17.txt")))))))
 
 (deftest part2
-  (let [expected nil]
-    (is (= expected (part-2 (slurp (resource "2021/day-17-example.txt")))))))
+
+  (testing "intersects trench"
+    (is (= true (intersects-trench? [7 2] [20 30] [-10 -5]))))
+
+  (let [expected 112]
+    (is (= expected (part-2 (slurp (resource "2021/day-17-example.txt"))))))
+
+  (let [expected 3012]
+    (is (= expected (part-2 (slurp (resource "2021/day-17.txt")))))))
