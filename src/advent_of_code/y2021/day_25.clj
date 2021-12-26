25153a944323ea3ed5cc016a4fdc3f40070cf335
Osvalds Neiders <osvalds.neiders@ui.com>
2021-12-26 17:39:57 +0200

    Day 25 pt1

--- src/advent_of_code/y2021/day_25.clj
+++ src/advent_of_code/y2021/day_25.clj
@@ -1,9 +1,56 @@
-(ns advent-of-code.y2021.day-25)
+(ns advent-of-code.y2021.day-25
+  (:require [clojure.string :as str]
+            [clojure.pprint :refer [pprint]]))
+
+(defn parse [input]
+  (mapv #(str/split % #"") (str/split-lines input)))
+
+(defn move-row-east [row]
+  (let [xs (range (count row))]
+    (reduce
+      (fn [result-row x]
+        (let [cc (get row x)
+              ncc (get row (rem (+ x 1) (count row)))]
+          (if (and (= cc ">") (= ncc "."))
+            (-> result-row
+              (assoc x ".")
+              (assoc (rem (inc x) (count row)) ">"))
+            result-row)))
+      row
+      xs)))
+
+(defn move-east [board]
+  (reduce
+    (fn [result row]
+      (conj result (move-row-east row)))
+    []
+    board))
+
+(defn move-south [board]
+  (let [yrange (range (count board))
+        xrange (range (count (first board)))
+        coords (for [y yrange x xrange] [y x])]
+    (reduce
+      (fn [result-board [y x]]
+        (let [cc (get-in board [y x])
+              ncc (get-in board [(rem (inc y) (count board)) x])]
+          (if (and (= cc "v") (= ncc "."))
+            (-> result-board
+              (assoc-in [y x] ".")
+              (assoc-in [(rem (inc y) (count result-board)) x] "v"))
+            result-board)))
+      board
+      coords)))
 
 (defn part-1
   "Day 25 Part 1"
   [input]
-  input)
+  (loop [i 0
+         board (parse input)]
+    (let [next-board (move-south (move-east board))]
+      (if (= board next-board)
+        (+ 1 i)
+        (recur (inc i) next-board)))))
 
 (defn part-2
   "Day 25 Part 2"
--- test/advent_of_code/y2021/day_25_test.clj
+++ test/advent_of_code/y2021/day_25_test.clj
@@ -1,10 +1,13 @@
 (ns advent-of-code.y2021.day-25-test
   (:require [clojure.test :refer [deftest testing is]]
-            [advent-of-code.y2021.day-25 :refer [part-1 part-2]]
+            [advent-of-code.y2021.day-25 :refer :all]
             [clojure.java.io :refer [resource]]))
 
 (deftest part1
-  (let [expected nil]
+  (let [expected 58]
+    (is (= expected (part-1 (slurp (resource "2021/day-25-example.txt"))))))
+
+  (let [expected 509]
     (is (= expected (part-1 (slurp (resource "2021/day-25.txt")))))))
 
 (deftest part2
